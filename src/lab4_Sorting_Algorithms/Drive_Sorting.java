e25b6f7208dd4e6240f02ecc0d545a8c78b1face
TheSpaceMan915 <97474520+TheSpaceMan915@users.noreply.github.com>
2022-04-11 20:29:32 +0300

    lab4 v1.1
    Haven't finished the first task yet

--- src/lab4_Sorting_Algorithms/Drive_Sorting.java
+++ src/lab4_Sorting_Algorithms/Drive_Sorting.java
@@ -7,11 +7,17 @@ public class Drive_Sorting {
     static private int m_iteration_number;
     static private int m_comparison_number;
     static private int m_swap_number;
+    static private long m_start_time;
+    static private long m_end_time;
+    static private double m_duration;
 
     static {
         m_iteration_number = 0;
         m_comparison_number = 0;
         m_swap_number = 0;
+        m_start_time = 0;
+        m_end_time = 0;
+        m_duration = 0;
     }
 
     //Templates for sort algorithms
@@ -43,7 +49,7 @@ public class Drive_Sorting {
 
         Comparable numbers_arr[] = new Comparable[arr_size];
         int rand = 0;
-        int min_value = -100, max_value = 100;
+        int min_value = -10000, max_value = 10000;
 
         for (int i = 0;i< numbers_arr.length;i++)
         {
@@ -63,16 +69,18 @@ public class Drive_Sorting {
 
     public static void BubbleSort(Comparable[] arr)
     {
+        m_start_time = System.nanoTime();
+        ZeroAllCounters();
+
         int i = arr.length - 1;
         boolean flag = false;
 
         while (i > 0)
         {
             int k = 0;
-
+            m_iteration_number++;
             while (k < i)
             {
-                m_iteration_number++;
                 if (less(arr[k+1],arr[k]))
                 {
                     exch(arr,k+1,k);
@@ -84,9 +92,16 @@ public class Drive_Sorting {
             {break;}
             i--;
         }
+        m_end_time = System.nanoTime();
+        m_duration = (double) (m_end_time - m_start_time)/1000000000;   //to get time in seconds
     }
 
-    public static void quicksort(Comparable[] arr) {qs(arr, 0, arr.length - 1);}
+    public static void quicksort(Comparable[] arr)
+    {
+        m_start_time = System.nanoTime();
+        ZeroAllCounters();
+        qs(arr, 0, arr.length - 1);
+    }
 
     public static void qs(Comparable[] arr, int l, int r) {
         if (l >= r) {
@@ -101,6 +116,7 @@ public class Drive_Sorting {
     public static int partition(Comparable[] arr, int l, int r) {
         Comparable pivot = arr[r];
         int i = l - 1;
+        m_iteration_number++;
 
         for (int j = l; j < r; j++) {
 
@@ -117,11 +133,15 @@ public class Drive_Sorting {
 
     public static void SelectionSort(Comparable[] arr)
     {
+        m_start_time = System.nanoTime();
+        ZeroAllCounters();
+
         int min_ind = 0;
         int temp = 0;
         for (int i = 0; i < arr.length - 1; i++)
         {
             min_ind = i;
+            m_iteration_number++;
             for (int j = i + 1; j < arr.length;j++)
             {
                 if (less(arr[j],arr[min_ind]))
@@ -133,8 +153,13 @@ public class Drive_Sorting {
 
     public static void InsertionSort(Comparable[] arr)
     {
+        m_start_time = System.nanoTime();
+        ZeroAllCounters();
+
         for (int i = 1; i < arr.length; i++)
         {
+            m_iteration_number++;
+
             for ( int j = i; j > 0; j--)
             {
                 if (less(arr[j],arr[j-1]))
@@ -149,9 +174,11 @@ public class Drive_Sorting {
     public static void main(String[] args) {
 
         System.out.println("№1");
-        System.out.println("-----------------------------------------------------------------------------");
-        System.out.printf("%10s 10s %10s %10s","Array size","Iteration number","Swap number","Comparison_number" + '\n');
-        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("Bubble Sort");
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.printf("%25s %25s %25s %25s %25s","Array size","Duration, seconds","Iteration number","Swap number","Comparison number");
+        System.out.println();
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------");
 
         Comparable[] arr_20 = CreateArray(20);
         Comparable[] arr_500 = CreateArray(500);
@@ -161,6 +188,29 @@ public class Drive_Sorting {
         Comparable[] arr_10000 = CreateArray(10000);
 
         BubbleSort(arr_20);
-        PrintArray(arr_20);
+        System.out.printf("%25s %25s %25s %25s %25s",arr_20.length,m_duration,m_iteration_number,m_swap_number,m_comparison_number);
+        System.out.println();
+
+        BubbleSort(arr_500);
+        System.out.printf("%25s %25s %25s %25s %25s",arr_500.length,m_duration,m_iteration_number,m_swap_number,m_comparison_number );
+        System.out.println();
+
+        BubbleSort(arr_1000);
+        System.out.printf("%25s %25s %25s %25s %25s",arr_1000.length,m_duration,m_iteration_number,m_swap_number,m_comparison_number );
+        System.out.println();
+
+        BubbleSort(arr_3000);
+        System.out.printf("%25s %25s %25s %25s %25s",arr_3000.length,m_duration,m_iteration_number,m_swap_number,m_comparison_number );
+        System.out.println();
+
+        BubbleSort(arr_5000);
+        System.out.printf("%25s %25s %25s %25s %25s",arr_5000.length,m_duration,m_iteration_number,m_swap_number,m_comparison_number );
+        System.out.println();
+
+        BubbleSort(arr_10000);
+        System.out.printf("%25s %25s %25s %25s %25s",arr_10000.length,m_duration,m_iteration_number,m_swap_number,m_comparison_number );
+        System.out.println();
+
+
     }
 }
