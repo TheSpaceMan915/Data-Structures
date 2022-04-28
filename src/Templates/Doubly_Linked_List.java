f9f90263cb10a38638168ad513fa48720fe571cb
TheSpaceMan915 <97474520+TheSpaceMan915@users.noreply.github.com>
2022-04-29 00:34:34 +0300

    lab8 v1.1
    Haven't written the delete function in Doubly Linked List

--- src/Templates/Doubly_Linked_List.java
+++ src/Templates/Doubly_Linked_List.java
@@ -2,7 +2,7 @@ package Templates;
 
 public class Doubly_Linked_List<Item> {
 
-    private Node m_head = null;
+    private Node m_head;
 
     private int m_current_items_number = 0;
 
@@ -27,6 +27,8 @@ public class Doubly_Linked_List<Item> {
         m_head = temp;             //make a new node the head
     }
 
+    //The function inserts an item after a certain node
+    /*
     public void insertItemAfter(Item obj,Node previous)
     {
         Node new_node = new Node();
@@ -39,6 +41,37 @@ public class Doubly_Linked_List<Item> {
 
         if (new_node.m_next != null)
         { new_node.m_next.m_prev = new_node; }      //connect a new node with the node after it
+    }*/
+
+    public void insert(Item obj, int pos)
+    {
+        Node new_node = new Node();
+        new_node.m_item = obj;
+
+        if (pos < 1)
+        { System.out.println("You can't insert the element at this position. Try again"); }
+        else if (pos == 1)
+        {
+            new_node.m_next = m_head;       //make an added element the head
+            m_head.m_prev = new_node;       //because we add at the first position in the list
+            m_head = new_node;
+        }
+        else
+        {
+            Node iterator = new Node();
+            iterator = m_head;
+
+            for (int i = 1; i < pos - 1; i++)       //iterate to the node previous to the position
+            { iterator = iterator.m_next; }
+
+            new_node.m_next = iterator.m_next;              // connect a new node with the node that the previous one pointed to
+
+            if (new_node.m_next != null)
+            { iterator.m_next.m_prev = new_node; }          //connect a new node with the node after it
+
+            new_node.m_prev = iterator;                     //connect a new node with the previous one
+            iterator.m_next = new_node;                     //connect the previous node with a new node
+        }
     }
 
     public static void main(String[] args) {
@@ -47,6 +80,7 @@ public class Doubly_Linked_List<Item> {
 
         list_obj.addItemAtTheFront(32);
         list_obj.addItemAtTheFront(54);
+        list_obj.insert(90,2);
 
 
         System.out.println("Check");
