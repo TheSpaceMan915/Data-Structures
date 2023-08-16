ee35e7c6a5b8507dda2d6e87871cfba74e31765d
Nikita Kolychev <97474520+TheSpaceMan915@users.noreply.github.com>
2023-08-16 09:43:27 +0300

    Update Fixed_Capacity_Stack

--- src/Algorithms_The_Book/Fixed_Capacity_Stack.java
+++ src/Algorithms_The_Book/Fixed_Capacity_Stack.java
@@ -4,7 +4,6 @@ public class Fixed_Capacity_Stack {
 
     private Comparable arr[];
     private int m_size = 0;
-    private int number = 0;
 
     public Fixed_Capacity_Stack(int size)
     { arr = new Comparable[size]; }
