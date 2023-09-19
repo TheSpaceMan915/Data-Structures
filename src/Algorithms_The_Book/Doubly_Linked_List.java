6fc6c3f24d5db7f9f9ade4d5b8a08197fb01e7a0
Nikita Kolychev <97474520+TheSpaceMan915@users.noreply.github.com>
2023-09-19 10:28:58 +0300

    Update Doubly_Linked_List

--- src/Algorithms_The_Book/Doubly_Linked_List.java
+++ src/Algorithms_The_Book/Doubly_Linked_List.java
@@ -3,8 +3,8 @@ package Algorithms_The_Book;
 public class Doubly_Linked_List<Item> {
 
     private Node m_head;
-
-    private int m_current_items_number = 0;
+    private Node tail;
+    private int m_current_items_number;
 
     public class Node
     {
